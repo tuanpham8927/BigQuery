230d8d446d861e9476b5c9b99a5da638f77e40ea
Tuan Pham <tuanpham8927@gmail.com>
2026-05-20 15:20:02 +0000

    Removed the invalid input and update to take only plu_id with price > 0

--- Generate_convert_customer_list.sql
+++ Generate_convert_customer_list.sql
@@ -15,19 +15,20 @@
    BigQuery Studio will show each final SELECT as a separate result set.
 ========================================================= */
 
-
 -- =========================================================
 -- 1) Parameters
 -- =========================================================
 WITH
-params AS (
-  SELECT
-    DATE '2026-05-16' AS post_launch_end_date,
-    ['2033', '2034', '2032'] AS new_plu_id,
-    ['20','3012','3010','104','2010','2011','3011','24','2012','21'] AS old_plu_id,
-    DATE '2026-01-01' AS pre_launch_begin_date,
-),
-
+  params AS (
+    SELECT
+      DATE '2026-05-16' AS post_launch_end_date,
+      ['2033', '2034', '2032'] AS new_plu_id,
+      -- ['2033'] AS new_plu_id,
+      ['20', '3012', '3010', '2010', '2011', '3011', '24', '2012']
+        AS old_plu_id,
+      -- ['2011'] AS old_plu_id,
+      DATE '2026-01-01' AS pre_launch_begin_date,
+  ),
   test_store_launch_dates AS (
     SELECT
       '2MWAXy3FzKTvmSgn' AS test_store_id,
@@ -45,70 +46,71 @@ params AS (
     UNION ALL
     SELECT 'b4gu065sNyiZggy7', 'Whitesboro / Highway 377 N', DATE '2026-05-08'
   ),
--- =========================================================
--- 2) Shared transaction base
---    This table is reused by all later steps.
--- =========================================================
-
-
-transaction_base AS (
-  SELECT
-   s.test_store_id,
+  -- =========================================================
+  -- 2) Shared transaction base
+  --    This table is reused by all later steps.
+  -- =========================================================
+  transaction_base AS (
+    SELECT
+      s.test_store_id,
       s.test_store_name,
       s.launch_date,
-    t.check_hash,
-    t.accountNumberId,
-    t.storeId,
-    t.business_date,
-    t.check
-  FROM `migration2220.NormalizedTransactions_StoreTimeZone` t
-  JOIN test_store_launch_dates s
+      t.check_hash,
+      t.accountNumberId,
+      t.storeId,
+      t.business_date,
+      t.check
+    FROM `migration2220.NormalizedTransactions_StoreTimeZone` t
+    JOIN test_store_launch_dates s
       ON t.storeId = s.test_store_id
-  JOIN `backfill_dataset.StoresToStoreGroups` sg
-    ON t.storeId = sg.storeId
-   AND sg.storeGroupId = 'GoldenChick'
-  CROSS JOIN params p
-  WHERE
-    t.transaction_type IN (
-      'Pre Auth Request',
-      'Pre Auth Complete',
-      'Purchase'
-    )
-    AND t.check IS NOT NULL
-    AND t.business_date BETWEEN p.pre_launch_begin_date
-                            AND p.post_launch_end_date
-),
+    JOIN `backfill_dataset.StoresToStoreGroups` sg
+      ON
+        t.storeId = sg.storeId
+        AND sg.storeGroupId = 'GoldenChick'
+    CROSS JOIN params p
+    WHERE
+      t.transaction_type IN (
+        'Pre Auth Request',
+        'Pre Auth Complete',
+        'Purchase')
+      AND t.check IS NOT NULL
+      AND t.business_date
+        BETWEEN p.pre_launch_begin_date
+        AND p.post_launch_end_date
+  ),
 
--- =========================================================
--- 3) Customers who bought the old/pre-launch PLU
--- =========================================================
-pre_launch_order AS (
-  SELECT
-    t.accountNumberId,
-    t.check_hash,
-    JSON_VALUE(line_item, '$.p') AS plu_id,
-    JSON_VALUE(line_item, '$.n') AS item_name
-  FROM transaction_base t
-  CROSS JOIN UNNEST(JSON_QUERY_ARRAY(t.check, '$.ls')) AS line_item
-  CROSS JOIN params p
-  WHERE
-    t.business_date BETWEEN p.pre_launch_begin_date
-                        AND DATE_SUB(t.launch_date, INTERVAL 1 DAY)
-    AND JSON_VALUE(line_item, '$.p') IN UNNEST(p.old_plu_id)
-    -- AND t.storeId IN UNNEST(p.test_store_id)
-    AND SAFE_CAST(JSON_VALUE(line_item, '$.pr') AS NUMERIC) != 0
-),
-pre_launch_order_customer AS (
-  SELECT
-    accountNumberId,
-    COUNT(DISTINCT check_hash) AS visit_number
-  FROM pre_launch_order
-  GROUP BY accountNumberId
-)
-
+  -- =========================================================
+  -- 3) Customers who bought the old/pre-launch PLU
+  -- =========================================================
+  pre_launch_order AS (
+    SELECT
+      t.accountNumberId,
+      t.check_hash,
+      t.test_store_id,
+      JSON_VALUE(line_item, '$.p') AS plu_id,
+      JSON_VALUE(line_item, '$.n') AS item_name
+    FROM transaction_base t
+    CROSS JOIN UNNEST(JSON_QUERY_ARRAY(t.check, '$.ls')) AS line_item
+    CROSS JOIN params p
+    WHERE
+      t.business_date
+        BETWEEN p.pre_launch_begin_date
+        AND DATE_SUB(t.launch_date, INTERVAL 1 DAY)
+      AND JSON_VALUE(line_item, '$.p') IN UNNEST(p.old_plu_id)
+      AND SAFE_CAST(JSON_VALUE(line_item, '$.pr') AS NUMERIC) > 0
+      AND SAFE_CAST(JSON_VALUE(line_item, '$.q') AS INT64) > 0
+  ),
+  pre_launch_order_customer AS (
+    SELECT
+      accountNumberId,
+      test_store_id,
+      COUNT(DISTINCT check_hash) AS visit_number
+    FROM pre_launch_order
+    GROUP BY accountNumberId, test_store_id
+  )
+-- SELECT accountNumberId FROM pre_launch_order_customer ORDER BY visit_number DESC
 ,
-
-
+-- SELECT * FROM migration2220.golden_chick_item_1c_step1_prior_old_item_buyers_6store_v1
 -- =========================================================
 -- 4) Of those pre-launch customers,
 --    find who returned during post-launch window.
@@ -119,22 +121,25 @@ pre_to_post_launch_population AS (
     COUNT(DISTINCT t.check_hash) AS visit_number
   FROM transaction_base t
   JOIN pre_launch_order_customer pre
-    ON t.accountNumberId = pre.accountNumberId
+    ON
+      t.accountNumberId = pre.accountNumberId
+      AND t.test_store_id = pre.test_store_id
   CROSS JOIN params p
   WHERE
-    t.business_date BETWEEN t.launch_date
-                        AND p.post_launch_end_date
+    t.business_date
+    BETWEEN t.launch_date
+    AND p.post_launch_end_date
   GROUP BY t.accountNumberId
-)
---SELECT * FROM pre_to_post_launch_population ORDER BY visit_number DESC;
-,
+)  -- SELECT * FROM pre_to_post_launch_population p
+  ,
 -- =========================================================
 -- 5) Of those returned customers,
 --    find who bought the new/post-launch PLU.
 -- =========================================================
 pre_to_post_new_plu_order AS (
   SELECT
-    pop.accountNumberId,t.check_hash,
+    pop.accountNumberId,
+    t.check_hash,
     JSON_VALUE(line_item, '$.p') AS plu_id,
     JSON_VALUE(line_item, '$.n') AS item_name
   FROM transaction_base t
@@ -143,11 +148,14 @@ pre_to_post_new_plu_order AS (
   CROSS JOIN params p
   CROSS JOIN UNNEST(JSON_QUERY_ARRAY(t.check, '$.ls')) AS line_item
   WHERE
-    t.business_date BETWEEN t.launch_date
-                        AND p.post_launch_end_date
+    t.business_date
+      BETWEEN t.launch_date
+      AND p.post_launch_end_date
     AND t.check IS NOT NULL
-    AND JSON_VALUE(line_item, '$.p') IN UNNEST (p.new_plu_id)
-    AND SAFE_CAST(JSON_VALUE(line_item, '$.pr') AS NUMERIC) != 0
+    AND JSON_VALUE(line_item, '$.p') IN UNNEST(p.new_plu_id)
+    AND SAFE_CAST(JSON_VALUE(line_item, '$.pr') AS NUMERIC) > 0
+    AND SAFE_CAST(JSON_VALUE(line_item, '$.q') AS INT64) > 0
+  -- AND SAFE_CAST(JSON_VALUE(line_item, '$.p') AS NUMERIC) != 0
 ),
 new_plu_order_customer AS (
   SELECT
@@ -156,10 +164,7 @@ new_plu_order_customer AS (
   FROM pre_to_post_new_plu_order
   GROUP BY accountNumberId
 )
-
-
 SELECT
   *
 FROM new_plu_order_customer
 ORDER BY visit_number DESC;
-
